f19cd75ce9ef7358654f1250a36ec485c3ffc7d6
Suraj Omar <100358484+suraj5621@users.noreply.github.com>
2024-02-04 18:14:18 +0530

    middleware --> use vs get

---
+++ middleware/app.js
@@ -0,0 +1,47 @@
+// pcr shrinkage
+const express = require('express');
+const app = express();
+
+
+// app.use('/',(req,res)=>{
+//     res.send("I am home")
+// })
+
+
+// app.use('/cat' , (req ,res)=>{
+//     res.send("you cat");
+// });
+
+
+
+app.get('/',(req,res)=>{
+    res.send('this is home page');
+})
+
+app.get('/cat',(req,res)=>{
+    res.send('this is cat page');
+})
+
+
+
+
+// Universal get astric --> universal route ---> generaly use for error page
+
+// app.get('*',(req,res)=>{
+//     res.send('this is error page');
+// })
+
+
+
+
+// by default aap koi bhi request bhejte ho wo kon se method pr jaati hai ---> get
+
+
+
+
+
+// nodemon install kro  tbb npm start se run hoga code or nodemon app.js se bhi run ho jayega 
+
+app.listen(8080, ()=>{
+    console.log('listening on port 8080');
+});
